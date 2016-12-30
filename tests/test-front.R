212015a432297989f3791d5276a2811eacd6804c
Ju Yeong Kim <jkim2345@fredhutch.org>
2016-12-30 14:11:36 -0800

    Add tests for checking contents in each module

--- tests/test-front.R
+++ tests/test-front.R
@@ -11,11 +11,22 @@ test_that("can connect to website", {
 test_that("'public data summary' module is present", {
   webElems <- remDr$findElements(using = "id", value = "Summary")
   expect_equal(length(webElems), 1)
+  
+  webElems <- remDr$findElements(using = "css selector", value = "tr")
+  expect_equal(length(webElems), 13)
+  
+  parsedTab <- readHTMLTable(htmlParse(remDr$getPageSource()[[1]]), stringsAsFactors = F)[[1]]
+  namesCol <- c("Studies", "Participants", "\u00A0", "CyTOF", "ELISA", "ELISPOT", 
+                "Flow Cytometry", "Gene Expression", "HAI", "HLA Typing", "MBAA", 
+                "Neutralizing Antibody", "PCR")
+  expect_equal(parsedTab[, 1], namesCol)
+  expect_equal(sum(parsedTab[, 2] > 0), 13)
 })
 
 test_that("'recent announcements' module is present", {
   webElems <- remDr$findElements(using = "id", value = "News")
   expect_equal(length(webElems), 1)
+  expect_true(webElems[[1]]$getElementText()[[1]] != "")
 })
 
 test_that("can log in", {
