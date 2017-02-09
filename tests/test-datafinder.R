1c2bea8563b3cd1c3fd96cb4512f764e54c93794
Ju Yeong Kim <jkim2345@fredhutch.org>
2017-02-09 13:57:40 -0800

    Initialize test file for data finder page

---
+++ tests/test-datafinder.R
@@ -0,0 +1,22 @@
+context("data finder page")
+
+if (!exists("ISR_login")) source("initialize.R")
+
+test_that("can connect to data finder page", {
+  pageURL <- paste0(siteURL, "/project/Studies/begin.view")
+  remDr$navigate(pageURL)
+  if (remDr$getTitle()[[1]] == "Sign In") {
+    id <- remDr$findElement(using = "id", value = "email")
+    id$sendKeysToElement(list(ISR_login))
+    
+    pw <- remDr$findElement(using = "id", value = "password")
+    pw$sendKeysToElement(list(ISR_pwd))
+    
+    loginButton <- remDr$findElement(using = "class", value = "labkey-button")
+    loginButton$clickElement()
+    
+    Sys.sleep(1)
+  }
+  pageTitle <- remDr$getTitle()[[1]]
+  expect_equal(pageTitle, "Overview: /Studies")
+})
