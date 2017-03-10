0c1a106b64dcb97f72346d0406275e414c61e938
Ju Yeong Kim <jkim2345@fredhutch.org>
2017-03-10 15:57:09 -0800

    Add simples tests for modules

--- tests/test-modules-gee.R
+++ tests/test-modules-gee.R
@@ -21,3 +21,26 @@ test_that("can connect to the page", {
   pageTitle <- remDr$getTitle()[[1]]
   expect_equal(pageTitle, "Gene Expression Explorer: /Studies/SDY269")
 })
+
+test_that("'Gene Expression Explorer' module is present", {
+  dataExplorer <- remDr$findElements(using = "id", value = "ext-comp-1084")
+  expect_equal(length(dataExplorer), 1)
+})
+
+test_that("tabs are present", {
+  inputView_tab <- remDr$findElements(using = "id", value = "ext-comp-1081__ext-comp-1061")
+  expect_equal(length(inputView_tab), 1)
+  expect_equal(inputView_tab[[1]]$getElementText()[[1]], "Input / View")
+  
+  data_tab <- remDr$findElements(using = "id", value = "ext-comp-1081__ext-comp-1072")
+  expect_equal(length(data_tab), 1)
+  expect_equal(data_tab[[1]]$getElementText()[[1]], "Data")
+  
+  about_tab <- remDr$findElements(using = "id", value = "ext-comp-1081__ext-comp-1082")
+  expect_equal(length(about_tab), 1)
+  expect_equal(about_tab[[1]]$getElementText()[[1]], "About")
+  
+  help_tab <- remDr$findElements(using = "id", value = "ext-comp-1081__ext-comp-1083")
+  expect_equal(length(help_tab), 1)
+  expect_equal(help_tab[[1]]$getElementText()[[1]], "Help")
+})
--- tests/test-modules-irp.R
+++ tests/test-modules-irp.R
@@ -20,3 +20,26 @@ test_that("can connect to the page", {
   pageTitle <- remDr$getTitle()[[1]]
   expect_equal(pageTitle, "Immune Response Predictor: /Studies/SDY269")
 })
+
+test_that("'Immune Response Predictor' module is present", {
+  IRP <- remDr$findElements(using = "id", value = "ext-comp-1076")
+  expect_equal(length(IRP), 1)
+})
+
+test_that("tabs are present", {
+  input_tab <- remDr$findElements(using = "id", value = "ext-comp-1073__ext-comp-1051")
+  expect_equal(length(input_tab), 1)
+  expect_equal(input_tab[[1]]$getElementText()[[1]], "Input")
+  
+  data_tab <- remDr$findElements(using = "id", value = "ext-comp-1073__ext-comp-1062")
+  expect_equal(length(data_tab), 1)
+  expect_equal(data_tab[[1]]$getElementText()[[1]], "View")
+  
+  about_tab <- remDr$findElements(using = "id", value = "ext-comp-1073__ext-comp-1074")
+  expect_equal(length(about_tab), 1)
+  expect_equal(about_tab[[1]]$getElementText()[[1]], "About")
+  
+  help_tab <- remDr$findElements(using = "id", value = "ext-comp-1073__ext-comp-1075")
+  expect_equal(length(help_tab), 1)
+  expect_equal(help_tab[[1]]$getElementText()[[1]], "Help")
+})
