332adf9580aa94983bc48685852edc71d141cb7e
Ju Yeong Kim <jkim2345@fredhutch.org>
2017-03-16 14:10:37 -0700

    Add tests to check datasets in both study and project levels

--- tests/test-data.R
+++ tests/test-data.R
@@ -20,3 +20,102 @@ test_that("can connect to the page", {
   pageTitle <- remDr$getTitle()[[1]]
   expect_equal(pageTitle, "Clinical and Assay Data: /Studies/SDY269")
 })
+
+test_that("List of datasets is present", {
+  treeview_table <- remDr$findElements(using = "css selector", value = "table[id^=treeview-]")
+  expect_equal(length(treeview_table), 1)
+})
+
+test_that("Relevant datasets are available", {
+  rows <- remDr$findElements(using = "class", value = "x4-tree-node-text")
+  expect_gt(length(rows), 0)
+  
+  rows_text <- unlist(lapply(rows, function(x) x$getElementText()[[1]]))
+  expected_text <- c("Assays",
+                     "Differential gene expression analysis results",
+                     "Enzyme-linked immunosorbent assay (ELISA)",
+                     "Enzyme-Linked ImmunoSpot (ELISPOT)",
+                     "Flow cytometry analyzed results",
+                     "Hemagglutination inhibition (HAI)",
+                     # "Human leukocyte antigen (HLA) typing",
+                     # "Killer cell immunoglobulin-like receptors (KIR) typing",
+                     # "Multiplex bead array assay",
+                     # "Neutralizing antibody titer",
+                     "Polymerisation chain reaction (PCR)",
+                     # "Gene Expression",
+                     # "Differential Expression Analysis",
+                     "Raw data files",
+                     # "FCS control files",
+                     "FCS sample files",
+                     "Gene expression microarray data files",
+                     "Subject",
+                     "Cohort membership",
+                     "Demographics")
+  
+  expect_equal(setdiff(rows_text, expected_text), character(0), 
+               info = paste(c("Unexpected datasets:", setdiff(rows_text, expected_text)), collapse = "\n"))
+  
+  expect_equal(setdiff(expected_text, rows_text), character(0), 
+               info = paste(c("Missing datasets:", setdiff(expected_text, rows_text)), collapse = "\n"))
+})
+
+################################################################################
+
+pageURL <- paste0(siteURL, "/project/Studies/begin.view?pageId=study.DATA_ANALYSIS")
+context(paste0("test-data.R: testing 'Clinical and Assay Data' page - project level (", pageURL, ")\n"))
+
+test_that("can connect to the page", {
+  remDr$navigate(pageURL)
+  if (remDr$getTitle()[[1]] == "Sign In") {
+    id <- remDr$findElement(using = "id", value = "email")
+    id$sendKeysToElement(list(ISR_login))
+    
+    pw <- remDr$findElement(using = "id", value = "password")
+    pw$sendKeysToElement(list(ISR_pwd))
+    
+    loginButton <- remDr$findElement(using = "class", value = "labkey-button")
+    loginButton$clickElement()
+    
+    while(remDr$getTitle()[[1]] == "Sign In") Sys.sleep(1)
+  }
+  pageTitle <- remDr$getTitle()[[1]]
+  expect_equal(pageTitle, "Clinical and Assay Data: /Studies")
+})
+
+test_that("List of datasets is present", {
+  treeview_table <- remDr$findElements(using = "css selector", value = "table[id^=treeview-]")
+  expect_equal(length(treeview_table), 1)
+})
+
+test_that("Relevant datasets are available", {
+  rows <- remDr$findElements(using = "class", value = "x4-tree-node-text")
+  expect_gt(length(rows), 0)
+  
+  rows_text <- unlist(lapply(rows, function(x) x$getElementText()[[1]]))
+  expected_text <- c("Assays",
+                     "Differential gene expression analysis results",
+                     "Enzyme-linked immunosorbent assay (ELISA)",
+                     "Enzyme-Linked ImmunoSpot (ELISPOT)",
+                     "Flow cytometry analyzed results",
+                     "Hemagglutination inhibition (HAI)",
+                     "Human leukocyte antigen (HLA) typing",
+                     "Killer cell immunoglobulin-like receptors (KIR) typing",
+                     "Multiplex bead array assay",
+                     "Neutralizing antibody titer",
+                     "Polymerisation chain reaction (PCR)",
+                     # "Gene Expression",
+                     # "Differential Expression Analysis",
+                     "Raw data files",
+                     "FCS control files",
+                     "FCS sample files",
+                     "Gene expression microarray data files",
+                     "Subject",
+                     "Cohort membership",
+                     "Demographics")
+  
+  expect_equal(setdiff(rows_text, expected_text), character(0), 
+               info = paste(c("Unexpected datasets:", setdiff(rows_text, expected_text)), collapse = "\n"))
+  
+  expect_equal(setdiff(expected_text, rows_text), character(0), 
+               info = paste(c("Missing datasets:", setdiff(expected_text, rows_text)), collapse = "\n"))
+})
