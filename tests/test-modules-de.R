88df0e5ee191696b5f4966584fd7dd88ba7c748a
J. Kim <jk@juyeongkim.com>
2017-05-30 16:42:19 -0700

    Update tests for Data Explorer

--- tests/test-modules-de.R
+++ tests/test-modules-de.R
@@ -33,14 +33,22 @@ test_that("tabs are present", {
 test_that("parameters are present", {
   Sys.sleep(3)
   
-  dataset_input <- remDr$findElements(using = "id", value = "ext-comp-1002")
+  # parameters
+  parameters <- remDr$findElements(using = "class", value = "ui-test-parameters")
+  expect_equal(length(parameters), 1)
+  
+  formItems <- parameters[[1]]$findChildElements(using = "class", value = "x-form-item")
+  expect_equal(length(formItems), 4)
+  
+  # parameters: dataset
+  dataset_input <- formItems[[1]]$findChildElements(using = "class", value = "ui-test-dataset")
   expect_equal(length(dataset_input), 1)
 
-  dataset_arrow <- remDr$findElements(using = "id", value = "ext-gen91")
+  dataset_arrow <- formItems[[1]]$findChildElements(using = "class", value = "x-form-arrow-trigger")
   expect_equal(length(dataset_arrow), 1)
   dataset_arrow[[1]]$clickElement()
   
-  dataset_list <- remDr$findElements(using = "id", value = "ext-gen94")
+  dataset_list <- remDr$findElements(using = "css selector", value = "div.x-combo-list[style*='visibility: visible']")
   expect_equal(length(dataset_list), 1)
   if (length(dataset_list) == 1) {
     dataset_actual <- strsplit(dataset_list[[1]]$getElementText()[[1]], "\n")[[1]]
@@ -58,57 +66,84 @@ test_that("parameters are present", {
                  info = paste(c("Missing datasets:", setdiff(dataset_expected, dataset_actual)), collapse = "\n"))
   }
 
-  dataset_clear <- remDr$findElements(using = "id", value = "ext-gen92")
+  dataset_clear <- formItems[[1]]$findChildElements(using = "class", value = "x-form-clear-trigger")
   expect_equal(length(dataset_clear), 1)
   dataset_clear[[1]]$clickElement()
 
-  plotType <- remDr$findElements(using = "id", value = "ext-comp-1003")
+  # parameters: plot type
+  plotType <- formItems[[2]]$findChildElements(using = "class", value = "ui-test-plottype")
   expect_equal(length(plotType), 1)
 
-  plotType_arrow <- remDr$findElements(using = "id", value = "ext-gen113")
+  plotType_arrow <- formItems[[2]]$findChildElements(using = "class", value = "x-form-arrow-trigger")
   expect_equal(length(plotType_arrow), 1)
   plotType_arrow[[1]]$clickElement()
   
-  plotType_list <- remDr$findElements(using = "id", value = "ext-gen116")
+  plotType_list <- remDr$findElements(using = "css selector", value = "div.x-combo-list[style*='visibility: visible']")
   expect_equal(plotType_list[[1]]$getElementText()[[1]], 
                "Auto\nBoxplot\nViolin plot\nHeatmap\nLineplot")
 
-  plotType_clear <- remDr$findElements(using = "id", value = "ext-gen114")
+  plotType_clear <- formItems[[2]]$findChildElements(using = "class", value = "x-form-clear-trigger")
   expect_equal(length(plotType_clear), 1)
   plotType_clear[[1]]$clickElement()
 
-  normalize <- remDr$findElements(using = "id", value = "ext-comp-1009")
+  # parameters: normalize
+  normalize <- remDr$findElements(using = "class", value = "ui-test-normalize")
   expect_equal(length(normalize), 1)
-
-  additionalOptions <- remDr$findElements(using = "id", value = "ext-gen60")
+  
+  # parameters: show strains
+  showStrains <- remDr$findElements(using = "class", value = "ui-test-showstrains")
+  expect_equal(length(showStrains), 1)
+  
+  # additional options
+  additionalOptions <- remDr$findElements(using = "class", value = "ui-test-additional-options")
   expect_equal(length(additionalOptions), 1)
-  additionalOptions[[1]]$clickElement()
+  
+  additionalOptions_header <- additionalOptions[[1]]$findChildElements(using = "class", value = "x-fieldset-header")
+  additionalOptions_header[[1]]$clickElement()
 
-  textSize <- remDr$findElements(using = "id", value = "ext-comp-1011")
+  interactivePlot <- additionalOptions[[1]]$findChildElements(using = "class", value = "ui-test-interactive")
+  expect_equal(length(interactivePlot), 1)
+  
+  textSize <- additionalOptions[[1]]$findChildElements(using = "class", value = "ui-test-textsize")
   expect_equal(length(textSize), 1)
 
-  horizontal <- remDr$findElements(using = "id", value = "ext-comp-1020")
+  horizontal <- additionalOptions[[1]]$findChildElements(using = "class", value = "ui-test-horizontal")
   expect_equal(length(horizontal), 1)
 
-  vertical <- remDr$findElements(using = "id", value = "ext-comp-1022")
+  vertical <- additionalOptions[[1]]$findChildElements(using = "class", value = "ui-test-vertical")
   expect_equal(length(vertical), 1)
 
-  plot_button <- remDr$findElements(using = "id", value = "ext-gen68")
+  # buttons
+  buttons_bar <- remDr$findElements(using = "class", value = "ui-test-buttons")
+  expect_equal(length(buttons_bar), 1)
+  
+  plot_button_table <- buttons_bar[[1]]$findChildElements(using = "class", value = "ui-test-plot")
+  expect_equal(length(plot_button_table), 1)
+  
+  plot_button <- plot_button_table[[1]]$findChildElements(using = "class", value = "x-btn-text")
   expect_equal(length(plot_button), 1)
+  expect_equal(plot_button[[1]]$getElementText()[[1]], "PLOT")
 
-  reset_button <- remDr$findElements(using = "id", value = "ext-gen70")
+  reset_button_table <- buttons_bar[[1]]$findChildElements(using = "class", value = "ui-test-reset")
+  expect_equal(length(reset_button_table), 1)
+  
+  reset_button <- reset_button_table[[1]]$findChildElements(using = "class", value = "x-btn-text")
   expect_equal(length(reset_button), 1)
+  expect_equal(reset_button[[1]]$getElementText()[[1]], "RESET")
 })
 
 test_that("loading dataset is working", {
-  dataset_arrow <- remDr$findElements(using = "id", value = "ext-gen91")
+  parameters <- remDr$findElements(using = "class", value = "ui-test-parameters")
+  formItems <- parameters[[1]]$findChildElements(using = "class", value = "x-form-item")
+
+  dataset_arrow <- formItems[[1]]$findChildElements(using = "class", value = "x-form-arrow-trigger")
   dataset_arrow[[1]]$clickElement()
   
-  dataset_list <- remDr$findElements(using = "id", value = "ext-gen94")
+  dataset_list <- remDr$findElements(using = "css selector", value = "div.x-combo-list[style*='visibility: visible']")
   dataset_elisa <- dataset_list[[1]]$findChildElements(using = "class", value = "x-combo-list-item")
   dataset_elisa[[1]]$clickElement()
   
-  while (remDr$findElements(using = "id", value = "ext-comp-1026")[[1]]$getElementText()[[1]] == "") {}
+  while (remDr$findElements(using = "class", value = "ui-test-cmpstatus")[[1]]$getElementText()[[1]] == "") {}
 
   tab_header <- remDr$findElements(using = "class", value = "x-tab-panel-header")
   tabs <- tab_header[[1]]$findChildElements(using = "css selector", value = "li[id^=ext-comp]")
@@ -122,33 +157,42 @@ test_that("loading dataset is working", {
     headers_text <- headers[[1]]$getElementText()[[1]]
   }
   expect_equal(headers_text, 
-               c("Participant ID", "Age Reported", "Gender", "Race", "Cohort", "Analyte", "Study Time Collected", "Study Time Collected Unit", "Value Reported", "Unit Reported"))
+               c("Participant ID", "Age Reported", "Gender", "Race", "Cohort", 
+                 "Analyte", "Study Time Collected", "Study Time Collected Unit", 
+                 "Value Reported", "Unit Reported"))
   
   tabs[[1]]$clickElement()
 })
 
 test_that("selecting plot type is working", {
-  plotType_arrow <- remDr$findElements(using = "id", value = "ext-gen113")
+  parameters <- remDr$findElements(using = "class", value = "ui-test-parameters")
+  formItems <- parameters[[1]]$findChildElements(using = "class", value = "x-form-item")
+  
+  plotType_arrow <- formItems[[2]]$findChildElements(using = "class", value = "x-form-arrow-trigger")
   plotType_arrow[[1]]$clickElement()
   
-  plotType_list <- remDr$findElements(using = "id", value = "ext-gen116")
+  plotType_list <- remDr$findElements(using = "css selector", value = "div.x-combo-list[style*='visibility: visible']")
   plotType_heatmap <- plotType_list[[1]]$findChildElements(using = "class", value = "x-combo-list-item")
   plotType_heatmap[[4]]$clickElement()
   
-  plotType <- remDr$findElements(using = "id", value = "ext-comp-1003")
+  plotType <- formItems[[2]]$findChildElements(using = "class", value = "ui-test-plottype")
   expect_equal(plotType[[1]]$getElementAttribute("value")[[1]], "Heatmap")
 })
 
 test_that("plot and reset buttons are working", {
-  plot_button <- remDr$findElements(using = "id", value = "ext-gen68")
+  buttons_bar <- remDr$findElements(using = "class", value = "ui-test-buttons")
+  
+  plot_button_table <- buttons_bar[[1]]$findChildElements(using = "class", value = "ui-test-plot")
+  plot_button <- plot_button_table[[1]]$findChildElements(using = "class", value = "x-btn-text")
   plot_button[[1]]$clickElement()
 
   # check if output is there
   while (length(remDr$findElements(using = "class", value = "ext-el-mask-msg")) != 0) {}
   visualization <- remDr$findElements(using = "css selector", value = "img[id*=imgModuleHtmlView_]")
   expect_equal(length(visualization), 1)
-
-  reset_button <- remDr$findElements(using = "id", value = "ext-gen70")
+  
+  reset_button_table <- buttons_bar[[1]]$findChildElements(using = "class", value = "ui-test-reset")
+  reset_button <- reset_button_table[[1]]$findChildElements(using = "class", value = "x-btn-text")
   reset_button[[1]]$clickElement()
 
   # check if plot is clear
