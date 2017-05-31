2f8fca86ddf7c64ef0c2322d20546c5cec0c1a66
Ju Yeong Kim <jkim2345@fredhutch.org>
2017-05-31 14:52:26 -0700

    Update tests for Immune Response Predictor

--- tests/test-modules-irp.R
+++ tests/test-modules-irp.R
@@ -33,96 +33,106 @@ test_that("tabs are present", {
 test_that("parameters are present and working", {
   Sys.sleep(3)
   
-  resp_input <- remDr$findElements(using = "id", value = "ext-comp-1002")
+  # parameters
+  parameters <- remDr$findElements(using = "class", value = "ui-test-parameters")
+  expect_equal(length(parameters), 1)
+  
+  formItems <- parameters[[1]]$findChildElements(using = "css selector", value = "div.x-form-item")
+  expect_equal(length(formItems), 6)
+  
+  # parameters: response
+  resp_input <- formItems[[1]]$findChildElements(using = "class", value = "ui-test-response")
   expect_equal(length(resp_input), 1)
   
-  resp_arrow <- remDr$findElements(using = "id", value = "ext-gen76")
+  resp_arrow <- formItems[[1]]$findChildElements(using = "class", value = "x-form-arrow-trigger")
   expect_equal(length(resp_arrow), 1)
   resp_arrow[[1]]$clickElement()
   
-  resp_list <- remDr$findElements(using = "id", value = "ext-gen234")
+  resp_list <- remDr$findElements(using = "css selector", value = "div.x-combo-list[style*='visibility: visible']")
   expect_equal(resp_list[[1]]$getElementText()[[1]], "HAI")
   
-  resp_clear <- remDr$findElements(using = "id", value = "ext-gen77")
+  resp_clear <- formItems[[1]]$findChildElements(using = "class", value = "x-form-clear-trigger")
   expect_equal(length(resp_clear), 1)
   
-  
-  pred_input <- remDr$findElements(using = "id", value = "ext-comp-1003")
+  # parameters: predictor
+  pred_input <- formItems[[2]]$findChildElements(using = "class", value = "ui-test-timepoint")
   expect_equal(length(pred_input), 1)
   
-  pred_arrow <- remDr$findElements(using = "id", value = "ext-gen95")
+  pred_arrow <- formItems[[2]]$findChildElements(using = "class", value = "x-form-arrow-trigger")
   expect_equal(length(pred_arrow), 1)
   pred_arrow[[1]]$clickElement()
   
-  pred_list <- remDr$findElements(using = "id", value = "ext-gen98")
+  pred_list <- remDr$findElements(using = "css selector", value = "div.x-combo-list[style*='visibility: visible']")
   expect_equal(pred_list[[1]]$getElementText()[[1]], 
                "0 days (2 cohorts)\n3 days (2 cohorts)\n7 days (2 cohorts)")
   pred_items <- pred_list[[1]]$findChildElements(using = "class", value = "x-combo-list-item")
   pred_items[[2]]$clickElement()
   Sys.sleep(1)
   
-  pred_clear <- remDr$findElements(using = "id", value = "ext-gen114")
+  pred_clear <- formItems[[2]]$findChildElements(using = "class", value = "x-form-clear-trigger")
   expect_equal(length(pred_clear), 1)
   
-  
-  train_input <- remDr$findElements(using = "id", value = "ext-comp-1004")
+  # parameters: training
+  train_input <- formItems[[3]]$findChildElements(using = "class", value = "ui-test-training")
   expect_equal(length(train_input), 1)
   
-  train_arrow <- remDr$findElements(using = "id", value = "ext-gen116")
+  train_arrow <- formItems[[3]]$findChildElements(using = "class", value = "x-form-arrow-trigger")
   expect_equal(length(train_arrow), 1)
   train_arrow[[1]]$clickElement()
   
-  train_list <- remDr$findElements(using = "id", value = "ext-gen118")
+  train_list <- remDr$findElements(using = "css selector", value = "div.x-combo-list[style*='visibility: visible']")
   expect_equal(train_list[[1]]$getElementText()[[1]], 
                "Select all\nLAIV group 2008\nTIV Group 2008")
   train_items <- train_list[[1]]$findChildElements(using = "class", value = "x-combo-list-item")
   train_items[[2]]$clickElement()
   Sys.sleep(1)
   
-  train_clear <- remDr$findElements(using = "id", value = "ext-gen117")
+  train_clear <- formItems[[3]]$findChildElements(using = "class", value = "x-form-clear-trigger")
   expect_equal(length(train_clear), 1)
   
-  
-  test_input <- remDr$findElements(using = "id", value = "ext-comp-1005")
+  # parameters: testing
+  test_input <- formItems[[4]]$findChildElements(using = "class", value = "ui-test-testing")
   expect_equal(length(test_input), 1)
   
-  test_arrow <- remDr$findElements(using = "id", value = "ext-gen139")
+  test_arrow <- formItems[[4]]$findChildElements(using = "class", value = "x-form-arrow-trigger")
   expect_equal(length(test_arrow), 1)
   test_arrow[[1]]$clickElement()
   
-  test_list <- remDr$findElements(using = "id", value = "ext-gen141")
+  test_list <- remDr$findElements(using = "css selector", value = "div.x-combo-list[style*='visibility: visible']")
   expect_equal(test_list[[1]]$getElementText()[[1]], 
                "Select all\nTIV Group 2008")
   test_items <- test_list[[1]]$findChildElements(using = "class", value = "x-combo-list-item")
   test_items[[2]]$clickElement()
   
-  test_clear <- remDr$findElements(using = "id", value = "ext-gen140")
+  test_clear <- formItems[[4]]$findChildElements(using = "class", value = "x-form-clear-trigger")
   expect_equal(length(test_clear), 1)
   
-  
-  dich <- remDr$findElements(using = "id", value = "ext-comp-1006")
+  # parameters: dichotomize
+  dich <- formItems[[5]]$findChildElements(using = "class", value = "ui-test-dichotomize")
   expect_equal(length(dich), 1, 
                info = "'Dichotomize' checkbox is not present.")
   dich[[1]]$clickElement()
   
-  dich_threshold <- remDr$findElements(using = "id", value = "ext-comp-1007")
+  dich_threshold <- formItems[[6]]$findChildElements(using = "class", value = "ui-test-dichotomize-value")
   expect_equal(length(dich_threshold), 1, 
                info = "'Dichotomization threshold' input box is not present.")
   
-  
-  additionalOptions <- remDr$findElements(using = "id", value = "ext-gen49")
+  # additional options
+  additionalOptions <- remDr$findElements(using = "class", value = "ui-test-additional-options")
   expect_equal(length(additionalOptions), 1)
-  additionalOptions[[1]]$clickElement()
   
-  AFC <- remDr$findElements(using = "id", value = "ext-comp-1011")
+  additionalOptions_header <- additionalOptions[[1]]$findChildElements(using = "class", value = "x-fieldset-header")
+  additionalOptions_header[[1]]$clickElement()
+  
+  AFC <- additionalOptions[[1]]$findChildElements(using = "class", value = "ui-test-foldchange")
   expect_equal(length(AFC), 1, 
                info = "'Absolute fold change' checkbox is not present.")
   
-  AFC_threshold <- remDr$findElements(using = "id", value = "ext-comp-1008")
+  AFC_threshold <- additionalOptions[[1]]$findChildElements(using = "class", value = "ui-test-foldchange-value")
   expect_equal(length(AFC_threshold), 1, 
                info = "'Absolute fold change threshold' input box is not present.")
   
-  GDE <- remDr$findElements(using = "id", value = "ext-comp-1013")
+  GDE <- additionalOptions[[1]]$findChildElements(using = "class", value = "ui-test-falsediscoveryrate")
   expect_equal(length(GDE), 1, 
                info = "'Use gene...' checkbox is not present.")
   
@@ -170,12 +180,12 @@ test_that("reset button is working", {
   buttons[[2]]$clickElement()
   
   # check if parameters are clear
-  pred_input <- remDr$findElements(using = "id", value = "ext-comp-1003")
+  pred_input <- remDr$findElements(using = "class", value = "ui-test-timepoint")
   expect_equal(pred_input[[1]]$getElementAttribute("value")[[1]], "Select...")
   
-  train_input <- remDr$findElements(using = "id", value = "ext-comp-1004")
+  train_input <- remDr$findElements(using = "class", value = "ui-test-training")
   expect_equal(train_input[[1]]$getElementAttribute("value")[[1]], "Select...")
   
-  test_input <- remDr$findElements(using = "id", value = "ext-comp-1005")
+  test_input <- remDr$findElements(using = "class", value = "ui-test-testing")
   expect_equal(test_input[[1]]$getElementAttribute("value")[[1]], "Select...")
 })
