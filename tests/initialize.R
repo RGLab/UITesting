2428c3f6b3738238fc2f61e5a5315b6f676945ab
Ju Yeong Kim <jkim2345@fredhutch.org>
2017-09-14 17:38:09 -0700

    Update test infrastructure to use local selenium server when specified

--- tests/initialize.R
+++ tests/initialize.R
@@ -4,68 +4,108 @@ library(testthat)
 library(XML)
 library(digest)
 
+# Get credentials
 ISR_login <- Sys.getenv("ISR_login")
 ISR_pwd <- Sys.getenv("ISR_pwd")
 SAUCE_USERNAME <- Sys.getenv("SAUCE_USERNAME")
 SAUCE_ACCESS_KEY <- Sys.getenv("SAUCE_ACCESS_KEY")
 
+# Get environment variables to determine test path
+seleniumServer <- ifelse(Sys.getenv("SELENIUM_SERVER") == "", "SAUCELABS", "LOCAL")
 machine <- ifelse(Sys.getenv("TRAVIS") == "true", "TRAVIS", "LOCAL")
 server <- ifelse(Sys.getenv("TRAVIS_BRANCH") == "master", "www", "test")
-
-build <- Sys.getenv("TRAVIS_BUILD_NUMBER")
-job <- Sys.getenv("TRAVIS_JOB_NUMBER")
-jobURL <- paste0("https://travis-ci.org/RGLab/UITesting/jobs/", Sys.getenv("TRAVIS_JOB_ID"))
-name <- ifelse(machine == "TRAVIS", 
-               paste0("UI testing `", server, "` by TRAVIS #", job, " ", jobURL), 
-               paste0("UI testing `", server, "` by ", Sys.info()["nodename"]))
-url <- ifelse(machine == "TRAVIS", "localhost", "ondemand.saucelabs.com")
-
-ip <- paste0(SAUCE_USERNAME, ":", SAUCE_ACCESS_KEY, "@", url)
-port <- ifelse(machine == "TRAVIS", 4445, 80)
-browserName <- ifelse(Sys.getenv("SAUCE_BROWSER") == "", 
-                      "chrome", 
-                      Sys.getenv("SAUCE_BROWSER"))
-extraCapabilities <- list(name = name, 
-                          build = build,
-                          username = SAUCE_USERNAME, 
-                          accessKey = SAUCE_ACCESS_KEY, 
-                          tags = list(machine, server))
-
-remDr <- remoteDriver$new(remoteServerAddr = ip, 
-                          port = port, 
-                          browserName = browserName, 
-                          version = "latest", 
-                          platform = "Windows 10", 
-                          extraCapabilities = extraCapabilities)
-remDr$open(silent = TRUE)
-ptm <- proc.time()
-
-cat("\nhttps://saucelabs.com/beta/tests/", remDr@.xData$sessionid, "\n", sep = "")
-if (machine == "TRAVIS") write(paste0("export SAUCE_JOB=", remDr@.xData$sessionid), "SAUCE")
-
-remDr$maxWindowSize()
-remDr$setImplicitWaitTimeout(milliseconds = 20000)
-
 siteURL <- paste0("https://", server, ".immunespace.org")
+browserName <- ifelse(Sys.getenv("TEST_BROWSER") == "",
+                      "chrome",
+                      Sys.getenv("TEST_BROWSER"))
+
+# Initiate Selenium server
+if (seleniumServer == "SAUCELABS") {
+  # With SauceLabs on Travis or local machine
+  
+  # 
+  build <- Sys.getenv("TRAVIS_BUILD_NUMBER")
+  
+  job <- Sys.getenv("TRAVIS_JOB_NUMBER")
+  jobURL <- paste0("https://travis-ci.org/RGLab/UITesting/jobs/", Sys.getenv("TRAVIS_JOB_ID"))
+  name <- ifelse(machine == "TRAVIS",
+                 paste0("UI testing `", server, "` by TRAVIS #", job, " ", jobURL),
+                 paste0("UI testing `", server, "` by ", Sys.info()["nodename"]))
+  
+  sauceUrl <- ifelse(machine == "TRAVIS", "localhost", "ondemand.saucelabs.com")
+  remoteServerAddr <- paste0(SAUCE_USERNAME, ":", SAUCE_ACCESS_KEY, "@", sauceUrl)
+  port <- ifelse(machine == "TRAVIS", 4445, 80)
+  
+  extraCapabilities <- list(name = name,
+                            build = build,
+                            username = SAUCE_USERNAME,
+                            accessKey = SAUCE_ACCESS_KEY,
+                            tags = list(machine, server))
+  
+  # Initiate a browser
+  remDr <- remoteDriver$new(remoteServerAddr = remoteServerAddr,
+                            port = port,
+                            browserName = browserName,
+                            version = "latest",
+                            platform = "Windows 10",
+                            extraCapabilities = extraCapabilities)
+  remDr$open(silent = TRUE)
+  
+  # Timer to match with SauceLabs browser (it's not so accurate)
+  ptm <- proc.time()
+  
+  #
+  cat("\nhttps://saucelabs.com/beta/tests/", remDr@.xData$sessionid, "\n", sep = "")
+  if (machine == "TRAVIS") write(paste0("export SAUCE_JOB=", remDr@.xData$sessionid), "SAUCE")
+  
+} else {
+  # With local machine
+  rs <- rsDriver(browser = browserName)
+  remDr <- rs$client
+}
+
+# 
+remDr$maxWindowSize()
+remDr$setTimeout(type = "implicit", milliseconds = 20000)
 
 
 # helper functions ----
 context_of <- function(file, what, url, level = NULL) {
   if (exists("ptm")) {
     elapsed <- proc.time() - ptm
-    timeStamp <- paste0("At ", floor(elapsed[3] / 60), " minutes ", 
+    timeStamp <- paste0("At ", floor(elapsed[3] / 60), " minutes ",
                         round(elapsed[3] %% 60), " seconds")
   } else {
     timeStamp <- ""
   }
   level <- ifelse(is.null(level), "", paste0(" (", level, " level) "))
   
-  msg <- paste0("\n", file, ": testing '", what, "' page", level, 
-                "\n", url, 
+  msg <- paste0("\n", file, ": testing '", what, "' page", level,
+                "\n", url,
                 "\n", timeStamp,
                 "\n")
   
   context(msg)
+}
+
+sleep_for <- function(seconds, condition = NULL) {
+  if (is.null(condition)) {
+    Sys.sleep(seconds)
+  } else {
+    counter <- 0
+    result <- eval(condition)
+    while (!result) {
+      Sys.sleep(1)
+      counter <- counter + 1
+      result <- eval(condition)
+      
+      if (counter == seconds) {
+        warning("can't wait for the condition to be true.")
+        break
+      }
+    }
+  }
+  NULL
 }
 
 
