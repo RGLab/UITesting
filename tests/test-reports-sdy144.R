c49afb1b7590ebb5c436c676146c1c01258502ae
Ju Yeong Kim <jkim2345@fredhutch.org>
2018-10-09 14:47:48 -0700

    Fix context typo

--- tests/test-reports-sdy144.R
+++ tests/test-reports-sdy144.R
@@ -1,7 +1,7 @@
 if (!exists("context_of")) source("initialize.R")
 
 pageURL <- paste0(siteURL, "/reports/Studies/SDY144/runReport.view?reportId=module%3ASDY144%2Freports%2Fschemas%2Fstudy%2Fdemographics%2FHAI_VN_vs_plasma_cells.Rmd")
-context_of(file = "test-reports-sdy269.R", 
+context_of(file = "test-reports-sdy144.R", 
            what = "SDY144 Report", 
            url = pageURL)
 
