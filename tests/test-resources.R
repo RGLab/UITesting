d59aabbbf07a97eda761cc7a9a0011e66031ba42
Evan Henrich <ehenrich@fredhutch.org>
2020-07-27 16:37:50 -0700

    ResourcesPage: Update expected report titles

--- tests/test-resources.R
+++ tests/test-resources.R
@@ -35,12 +35,12 @@ test_that("Highlighted Reports tab has correct elements", {
     headerText <- header$getElementText()
   }))
 
-  expectedHeaders <- c("SDY144",
-                       "SDY180",
-                       "SDY207",
-                       "SDY269",
-                       "ImmuneSignatures 1",
-                       "Lyoplate")
+  expectedHeaders <- c('Correlation of HAI/VN and plasma cell counts',
+                       'Measuring plasmablast abundance by multi-parameter flow cytometry',
+                       'Multiple custom reports related to CyTOF and Flow Cytometry Data',
+                       'Correlating HAI with flow cytometry and ELISPOT results',
+                       'Influenza Vaccine Meta-Analysis to define genetic predictors of vaccine response',
+                       'Standardizing Flow Cytometry Immunophenotyping from Reagents to Analyses:  Results of the Human ImmunoPhenotyping Consortium')
 
   expect_true(all.equal(actualHeaders, expectedHeaders))
 
