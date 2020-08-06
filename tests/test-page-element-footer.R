44f22267296d0ecbe77e47f9482b32eb5d67613d
Evan Henrich <ehenrich@fredhutch.org>
2020-08-06 16:25:56 -0700

    Separate footer and main menu due to saucelabs issue with second window in footer tests

--- tests/test-page-element-footer.R
+++ tests/test-page-element-footer.R
@@ -31,8 +31,6 @@ test_link <- function(element, linkText, linkTitle) {
 
 pageUrl <- paste0(site_url, "/project/Studies/begin.view?")
 
-# ------------ Footer ---------------
-
 context_of(
   file = "test-page-elements.R",
   what = "Site Footer",
@@ -74,36 +72,5 @@ test_that("Footer links work", {
 
   mapply(test_link, links, linkText, linkTitles)
 
-})
-
-# ------------- Main Menu -------------------
-
-context_of(
-  file = "test-page-elements.R",
-  what = "Main Menu",
-  url = pageUrl
-)
-
-test_that("`Main Menu` tab shows options properly", {
-  main_menu_tab <- remDr$findElements("css selector", "li[data-name=MainMenu]")
-  expect_length(main_menu_tab, 1)
-
-  if (length(main_menu_tab) == 1) {
-    main_menu_tab[[1]]$clickElement()
-    expect_equal(main_menu_tab[[1]]$getElementAttribute("class")[[1]], "dropdown open", info = "Does 'Main Menu' tab exist?")
-
-    menu_options_ul <- main_menu_tab[[1]]$findChildElements("css selector", "ul[id=list]")
-    menu_options_a <- menu_options_ul[[1]]$findChildElements("css selector", "a")
-    expect_equal(length(menu_options_a), 3)
-
-    link_names <- unlist(lapply(menu_options_a, function(x){
-      txt <- x$getElementText()
-    }))
-
-    expected_link_names <- c("Find Participants", "Resources", "About")
-    expect_equal(link_names, expected_link_names)
-
-    main_menu_tab[[1]]$clickElement()
-  }
 })
 
