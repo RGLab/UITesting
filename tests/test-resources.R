776097714ab7cde8238f6a13f31239a396bdcb89
J. Kim <jkim2345@fredhutch.org>
2021-05-13 09:45:33 -0700

    Update test expectation

--- tests/test-resources.R
+++ tests/test-resources.R
@@ -4,7 +4,7 @@ page_url <- paste0(site_url, "/project/Studies/begin.view?pageId=Resources")
 
 context_of("test-resourcesPage.R", "Resources Page", page_url)
 
-test_connection(remDr, page_url, "Studies: /Studies")
+test_connection(remDr, page_url, "Resources: /Studies")
 
 sleep_for(2)
 
